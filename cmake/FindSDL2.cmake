f401bf6113af19dff5dda3ea7a253cba565743e3
OlivLdff <olivier.ldff@gmail.com>
2017-11-30 19:14:34 +0100

    update cmakelist sdl

--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -1,3 +1,4 @@
+if(MSVC)
 if(NOT SDL2_DIR)
   set(SDL2_DIR "" CACHE PATH "SDL2 directory")
 endif()
@@ -127,3 +128,67 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL
                                   VERSION_VAR SDL2_VERSION_STRING)
 
 mark_as_advanced(SDL2_LIBRARY SDL2_INCLUDE_DIR)
+else(MSVC)
+# GNU sdl2
+if(SDL2_INCLUDE_DIR AND SDL2_LIBRARY)
+  set(SDL2_FOUND TRUE)
+else(SDL2_INCLUDE_DIR AND SDL2_LIBRARY)
+
+  #mac with port install
+  find_path(
+    SDL2_INCLUDE_DIR
+    SDL2/SDL.h
+    PATHS
+    /opt/local/include/
+    NO_DEFAULT_PATH)
+
+  find_library(
+    SDL2_LIBRARY 
+    sdl2
+    /opt/local/lib
+    NO_DEFAULT_PATH)
+
+
+  #linux 
+  find_path(
+    SDL2_INCLUDE_DIR
+    SDL2/SDL.h)
+  find_library(
+    SDL2_LIBRARY
+    SDL2)
+	
+  #windows
+  find_path(
+    SDL2_INCLUDE_DIR
+    SDL2/SDL.h
+    PATHS
+    "C:/Program Files/SDL"
+    NO_DEFAULT_PATH)
+
+  find_library(
+    SDL2_LIBRARY 
+    sdl2
+    "C:/Program Files/SDL/lib/x86"
+    NO_DEFAULT_PATH)
+	
+	#windows oliv
+  find_path(
+    SDL2_INCLUDE_DIR
+    SDL2/SDL.h
+    PATHS
+    "D:/Programmes/SDL2-2.0.5/include"
+    NO_DEFAULT_PATH)
+
+  find_library(
+    SDL2_LIBRARY 
+    sdl2
+    "D:/Programmes/SDL2-2.0.5/lib/x86"
+    NO_DEFAULT_PATH)
+
+
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(sdl2 DEFAULT_MSG SDL2_INCLUDE_DIR SDL2_LIBRARY )
+  mark_as_advanced(SDL2_INCLUDE_DIR SDL2_LIBRARY)
+endif(SDL2_INCLUDE_DIR AND SDL2_LIBRARY)
+endif(MSVC)
--- cmake/FindSDL2_image-old.cmake
+++ cmake/FindSDL2_image-old.cmake
@@ -1,4 +1,3 @@
-if(MSVC)
 # Distributed under the OSI-approved BSD 3-Clause License. See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
@@ -97,66 +96,3 @@ set(SDL2IMAGE_INCLUDE_DIR ${SDL2_IMAGE_INCLUDE_DIRS})
 set(SDL2IMAGE_FOUND ${SDL2_IMAGE_FOUND})
 
 mark_as_advanced(SDL2_IMAGE_LIBRARY SDL2_IMAGE_INCLUDE_DIR)
-else(MSVC)
-# GNU sdl2 image 
-if(SDL2IMAGE_INCLUDE_DIR AND SDL2IMAGE_LIBRARY)
-  set(SDL2IMAGE_FOUND TRUE)
-else(SDL2IMAGE_INCLUDE_DIR AND SDL2IMAGE_LIBRARY)
-
-  #mac with port install
-  find_path(
-    SDL2IMAGE_INCLUDE_DIR
-    SDL2/SDL_image.h
-    PATHS
-    /opt/local/include/
-    NO_DEFAULT_PATH)
-
-  find_library(
-    SDL2IMAGE_LIBRARY 
-    sdl2_image
-    /opt/local/lib
-    NO_DEFAULT_PATH)
-
-
-  #linux 
-  find_path(
-    SDL2IMAGE_INCLUDE_DIR
-    SDL2/SDL_image.h)
-  find_library(
-    SDL2IMAGE_LIBRARY
-    SDL2_image)
-	
-  #windows
-  find_path(
-    SDL2IMAGE_INCLUDE_DIR
-    SDL2/SDL_image.h
-    PATHS
-    "C:/Program Files/SDL"
-    NO_DEFAULT_PATH)
-
-  find_library(
-    SDL2IMAGE_LIBRARY 
-    sdl2_image
-    "C:/Program Files/SDL/lib/x86"
-    NO_DEFAULT_PATH)
-
-	#windows oliv
-  find_path(
-    SDL2IMAGE_INCLUDE_DIR
-    SDL2/SDL_image.h
-    PATHS
-    "D:/Programmes/SDL2-2.0.5/include"
-    NO_DEFAULT_PATH)
-
-  find_library(
-    SDL2IMAGE_LIBRARY 
-    sdl2_image
-    "D:/Programmes/SDL2-2.0.5/lib/x86"
-    NO_DEFAULT_PATH)
-
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(sdl2image DEFAULT_MSG SDL2IMAGE_INCLUDE_DIR SDL2IMAGE_LIBRARY )
-  mark_as_advanced(SDL2IMAGE_INCLUDE_DIR SDL2IMAGE_LIBRARY)
-endif(SDL2IMAGE_INCLUDE_DIR AND SDL2IMAGE_LIBRARY)
-endif(MSVC)
